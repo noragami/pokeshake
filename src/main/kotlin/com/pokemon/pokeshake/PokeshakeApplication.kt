5a878425ec62bb536b0eb6e485cb9bb852a4ed3d
noragami <yuumeikai@gmail.com>
2021-02-24 10:35:43 +0100

    backbone from spring initializr

---
+++ src/main/kotlin/com/pokemon/pokeshake/PokeshakeApplication.kt
@@ -0,0 +1,11 @@
+package com.pokemon.pokeshake
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PokeshakeApplication
+
+fun main(args: Array<String>) {
+	runApplication<PokeshakeApplication>(*args)
+}
---
+++ src/test/kotlin/com/pokemon/pokeshake/PokeshakeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pokemon.pokeshake
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PokeshakeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
